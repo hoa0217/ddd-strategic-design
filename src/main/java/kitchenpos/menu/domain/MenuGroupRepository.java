f2a74c9d9d358f59a90207d94944b6e57c5e9ed1
hoa0217 <gjwjdghk123@naver.com>
2024-06-02 03:54:06 +0900

    chore(kitchenpos): 메뉴 바운디드 컨텍스트 패키지 분리

--- src/main/java/kitchenpos/menu/domain/MenuGroupRepository.java
+++ src/main/java/kitchenpos/menu/domain/MenuGroupRepository.java
@@ -1,8 +1,9 @@
-package kitchenpos.domain;
+package kitchenpos.menu.domain;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import kitchenpos.menu.domain.MenuGroup;
 
 public interface MenuGroupRepository {
     MenuGroup save(MenuGroup menuGroup);
